edcb1b8f11d358da524616860d8bdbda71483ac7
Daniel Li <42878121+daniel88li@users.noreply.github.com>
2021-04-16 18:14:28 -0400

    Update README.md
    Add rules link
    
    Rebase commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       <h1>Conway's Game of Life</h1>
+      <h3>
+        Read the rules on{" "}
+        <a href={"https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life"}>
+          wikipedia
+        </a>
+      </h3>
       <Canvas />
     </div>
   );
